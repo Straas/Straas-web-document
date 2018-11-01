15f8da557fa48f4fc47776c85621ea3f8e2f150f
George Chang <georgechang0117gmail.com>
2018-11-01 11:02:31 +0800

    docs: modify assets

---
+++ StraaS APIs Documentation_files/application.self-bb7fb2b5c27afa24f8c166b7f86b543b21e8e83fd4456d86834aea6ed1202f74.js
@@ -0,0 +1,32 @@
+
+
+
+
+hljs.initHighlightingOnLoad();
+
+jQuery(function(){
+  $(document.body).scrollspy({target: '.sidebar', offset: $('.navbar').outerHeight(true)});
+  var $sideBar = $('.single-page .sidebar');
+  $sideBar.affix({
+    offset: {
+      top: function () { return this.top = $sideBar.offset().top - $('.docs-nav').height() - 30 }
+    }
+  })
+});
+
+$(document).ready(function() {
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 1500) {
+      $('#scroll-to-top').removeClass('hidden').affix({
+        offset: { top:100 }
+      });
+    } else {
+      $('#scroll-to-top').addClass('hidden')
+    }
+  });
+  
+  $("#scroll-to-top").click(function(){
+    $('html, body').animate({scrollTop : 0},500);
+    return false;
+  });
+});
